3141da64d47575c00b169c97645b1f8b308e382d
dylanmctigue <mctiguedm@g.cofc.edu>
2022-01-21 16:52:40 -0500

    Homework 1 commit

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -9,11 +9,13 @@ Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 """
 
+
 def calc_rec_area():
     length = eval(input("Enter the length: "))
     width = eval(input("Enter the width: "))
     area = length * width
     print("The area is", area)
+
 
 def calc_volume():
     length = eval(input("enter the length: "))
@@ -22,6 +24,7 @@ def calc_volume():
     volume = length * width * height
     print("The volume of the rectangular solid is", volume)
 
+
 def shooting_percentage():
     total_shots = eval(input("Enter the player's total shots: "))
     shots_made = eval(input("Enter how many shots the player made: "))
@@ -29,16 +32,19 @@ def shooting_percentage():
     round_shoot_percentage = round(shoot_percentage, 2)
     print("The shooting percentage is", round_shoot_percentage, "%")
 
+
 def coffee():
     pounds_coffee_purchase = eval(input("How many pounds of coffee would you like?"))
     total = ((10.50 + .86) * pounds_coffee_purchase) + 1.50
     print("Your total is $", total)
 
+
 def kilometers_to_miles():
-    kilometers_travled = eval(input("How many kilometers did you travel?"))
-    k_to_m = kilometers_travled / 1.61
+    kilometers_traveled = eval(input("How many kilometers did you travel?"))
+    k_to_m = kilometers_traveled / 1.61
     round_k_to_m = round(k_to_m, 2)
     print("That's", round_k_to_m, "miles!")
+
 
 if __name__ == '__main__':
     pass
