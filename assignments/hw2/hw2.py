0ec0c4f93adede736400ceed2bf3f105f2dbdf01
dylanmctigue <mctiguedm@g.cofc.edu>
2022-01-28 23:22:49 -0500

    Hw2 Commit

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,35 +1,58 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Dylan McTigue
+hw2.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: develop programs that do take user input and output answers
+doing arithmetic using math library.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 import math
 
 
 def sum_of_threes():
-    pass
+    upper_bound = eval(input("Enter the upper bound: "))
+    my_sum = 0
+    for i in range(0, upper_bound + 1, 3):
+        my_sum = my_sum + i
+    print(my_sum)
 
 
 def multiplication_table():
-    pass
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(i * j, end="\t")
+        print()
 
 
 def triangle_area():
-    pass
+    side_a = eval(input("Enter side a length: "))
+    side_b = eval(input("Enter side b length: "))
+    side_c = eval(input("Enter side c length: "))
+
+    form_1 = (side_a + side_b + side_c) / 2
+    form_2 = form_1 * (form_1 - side_a) * (form_1 - side_b) * (form_1 - side_c)
+    area = math.sqrt(form_2)
+    print("The area of the triangle is:", area)
 
 
 def sum_squares():
-    pass
+    lower_range = eval(input("enter lower range: "))
+    upper_range = eval(input("enter upper range: "))
+    total_sum = 0
+    for i in range(lower_range, upper_range + 1):
+        total_sum = total_sum + (i * i)
+    print("the sum of squares is", total_sum)
 
 
 def power():
-    pass
+    base = eval(input("enter base: "))
+    exponent = eval(input("enter exponent: "))
+    answer = 1
+    for i in range(exponent):
+        answer = answer * base
+    print(base, "^", exponent, "=", answer)
 
 
 if __name__ == '__main__':
