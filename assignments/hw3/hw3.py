f3ae5fe708062bbd8fb32852e9c171fb3a49bf21
dylanmctigue <mctiguedm@g.cofc.edu>
2022-02-04 21:27:37 -0500

    Hw3 committ

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,30 +1,61 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Dylan McTigue
+hw3.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Develop programs that take user input and give output doing arithmetic with loops. Such
+programs will find average with grades, keeps track of tip money in a jar being passed around,
+calculate square root using newton's method, output a sequence, and compute pi.
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is entirely my own work, but discussed with Mr. Baier.
 """
 
 
 def average():
-    pass
+    grades_to_enter = eval(input("How many grades will you enter?"))
+
+    acc_grade = 0
+
+    for i in range(grades_to_enter):
+        grade = eval(input("enter grade: "))
+        acc_grade = acc_grade + grade
+
+    grade_average = acc_grade / grades_to_enter
+
+    print("the average is", grade_average)
 
 
 def tip_jar():
-    pass
+
+    acc_tip = 0
+
+    for i in range(5):
+        donation = eval(input("How much would you like to donate?"))
+        acc_tip = acc_tip + donation
+
+    print("total tips: ", acc_tip)
 
 
 def newton():
-    pass
+    sqrt_num = eval(input("What number do you want to square root?"))
+    improvement = eval(input("How many times should we improve the approximation?"))
+
+    approx = sqrt_num
+
+    for i in range(improvement):
+        approx = (approx + (sqrt_num / approx)) / 2
+
+    print("the square root is approximately: ", approx)
 
 
 def sequence():
-    pass
+    term_num = eval(input("How many terms would you like?"))
+    x = 0
+    for i in range(term_num):
+        term = (i + 3) % (i + 4)
+        print(term, end=" ")
+        x = x + 4
+sequence()
 
 
 def pi():
